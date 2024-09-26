3e3a7fc0729b5a02f3225d2375144db734b7a2e3
SWEDHA V <62498400+swedha333@users.noreply.github.com>
2024-09-26 15:31:20 +0530

    sobel,canny,laplaciann.py

--- sobel,canny,laplaciann.py
+++ sobel,canny,laplaciann.py
@@ -1,46 +1,66 @@
-# In[1]:Import the packages
-# Reg no : 
-# Name : 
 import cv2
-import numpy as np
 import matplotlib.pyplot as plt
 
-# Load the image, Convert to grayscale and remove noise
-image1=cv2.imread ('downloads.jpg') 
-gray = cv2.cvtColor
-plt.title('GRAY IMAGE')
-plt.imshow(gray,cmap = 'gray')
+//TYPE THE CODE HERE TO READ THE IMAGE UISNG IMREAD 
+gray=cv2.cvtColor(img,)//CONVERT THE COLOR TO GRAY TO RGB
+gray = cv2.GaussianBlur(gray,(3,3),0)
 
-# SOBEL EDGE DETECTOR
-img = cv2.Gaussian(,(3,3),0)
 sobelx = cv2.Sobel(gray,cv2.CV_64F,1,0,ksize=5)
-sobely = cv2.Sobel
-sobelxy =cv2.Sobel()
-plt.figure(1)
-plt.subplot(2,2,1)
-plt.imshow(gray,cmap = 'gray')
-plt.title('Original'), plt.xticks([]), )
-
-plt.subplot(2,2,2)
-plt.imshow(sobelx,cmap='gray')
-plt.title('sobelx')
-plt.xticks([]), plt.yticks([])
-plt.subplot(2,2,
-plt.subplot(2,2,4)
-plt.imshow(sobelxy,cmap='gray')
-
-
-# In[2]:LAPLACIAN EDGE DETECTOR
-laplacian = cv2.Laplacian(gray,cv2.CV_64F)
-plt.imshow()
-plt.title('laplacian')
+plt.figure(figsize=(8,8))
+plt.subplot(1,2,1)
+plt.imshow(gray)
+plt.title("Original Image")
+plt.axis("off")
+plt.subplot(1,2,2)
+plt.imshow(sobelx)
+plt.title("Sobel X axis")
+plt.axis("off")
 plt.show()
 
+sobely = cv2.Sobel()//TYPE THE CODE HERE 
+plt.figure(figsize=(8,8))
+plt.subplot(1,2,1)
+plt.imshow(gray)
+plt.title("Original Image")
+plt.axis("off")
+plt.subplot(1,2,2)
+plt.imshow(sobely)
+plt.title("Sobel Y axis")
+plt.axis("off")
+plt.show()
 
-# In[3]: CANNY EDGE DETECTOR
-canny_edges = cv2.Canny(gray, 110, 140)
+sobelxy = cv2.Sobel(gray,cv2.CV_64F,1,1,ksize=5)
+plt.figure(figsize=(8,8))
+plt.subplot(1,2,1)
+plt.imshow(gray)
+plt.title("Original Image")
+plt.axis("off")
+//TYPE THE CODE HERE
+plt.imshow(sobelxy)
+plt.title("Sobel XY axis")
+plt.axis("off")
+plt.show()
 
+lap=cv2.Laplacian(gray,cv2.CV_64F)
+plt.figure(figsize=(8,8))
+plt.subplot(1,2,1)
+plt.imshow(gray)
+plt.title("Original Image")
+plt.axis("off")
+plt.subplot(1,2,2)
+//DISPLAY THE IMAGE USING IMSHOW 
+plt.title("Laplacian Edge Detector")
+plt.axis("off")
+plt.show()
 
-
-
-
+canny=cv2.Canny(gray,120,150)
+plt.figure(figsize=(8,8))
+plt.subplot(1,2,1)
+plt.imshow(gray)
+plt.title("Original Image")
+plt.axis("off")
+plt.subplot(1,2,2)
+plt.imshow(canny)
+//PROVIDE THE TITLE OF THE IMAGE DISPLAYED PAGE
+plt.axis("off")
+plt.show()
